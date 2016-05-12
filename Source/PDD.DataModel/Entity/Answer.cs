20ea813b2bde11e18c55a4121782c68398701300
saniyasembekova <ssaniyaya@gmail.com>
2016-05-12 19:15:01 +0600

    Заменила id на Id

--- Source/PDD.DataModel/Entity/Answer.cs
+++ Source/PDD.DataModel/Entity/Answer.cs
@@ -4,7 +4,7 @@ namespace PDD.DataModel.Entity
 {
     public class Answer
     {
-        public int id { get; set; }
+        public int Id { get; set; }
         public string Name { get; set; }
         public int QuestionId { get; set; }
         public virtual Question Question { get; set; }
